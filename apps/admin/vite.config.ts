32260b878d54ca6f43a52feb33c43d5c012828bc
grmkris <kristjan.grm1@gmail.com>
2024-01-15 00:13:36 +0100

    inital

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import * as path from "node:path";
 import {nodePolyfills} from "vite-plugin-node-polyfills";
 
 export default defineConfig({
--- apps/api/src/router.ts
+++ apps/api/src/router.ts
@@ -1,21 +1,3 @@
-import { z } from "zod";
-
-import {
-  eoas,
-  insertEoaSchema,
-  insertInvoiceSchema,
-  insertPaymentSchema,
-  insertSafeSchema,
-  invoices,
-  payments,
-  safes,
-  selectEoaSchema,
-  selectInvoiceSchema,
-  selectPaymentSchema,
-  selectSafeSchema,
-} from "ghommerce-schema/src/db/schema";
-import { db } from "./db/db";
-import { createRouterFactory } from "./lib/genericRouter";
 import { router } from "./lib/trpc";
 import { verifyWalletRouter } from "./routes/verifyWalletRouter";
 import { storeRouter } from "./routes/storeRouter";
@@ -23,85 +5,6 @@ import { ZeroExRouter } from "./routes/zeroExRouter";
 import { tokenRouter } from "./routes/tokenRouter";
 
 export const apiRouter = router({
-  invoices: createRouterFactory({
-    id: "invoices",
-    selectSchema: selectInvoiceSchema,
-    updateSchema: selectInvoiceSchema.partial().extend({ id: z.string() }),
-    insertSchema: insertInvoiceSchema.omit({ id: true }),
-    hooks: {
-      beforeInsert: (input, ctx) => {
-        if (!ctx.session?.user?.id) throw new Error("Unauthorized");
-        return {
-          ...input,
-          userId: ctx.session.user.id,
-        };
-      },
-    },
-    config: {
-      store: {
-        relation: "true",
-      },
-    },
-    table: invoices,
-    db: db,
-  }),
-  payments: createRouterFactory({
-    id: "payments",
-    selectSchema: selectPaymentSchema,
-    updateSchema: selectPaymentSchema.partial().extend({ id: z.string() }),
-    insertSchema: insertPaymentSchema,
-    hooks: {
-      beforeInsert: (input, ctx) => {
-        if (!ctx.session?.user?.id) throw new Error("Unauthorized");
-        return {
-          ...input,
-          userId: ctx.session.user.id,
-        };
-      },
-    },
-    table: payments,
-    db: db,
-  }),
-  eoas: createRouterFactory({
-    id: "eoas",
-    selectSchema: selectEoaSchema,
-    updateSchema: selectEoaSchema.partial().extend({ id: z.string() }),
-    insertSchema: insertEoaSchema,
-    hooks: {
-      beforeInsert: (input, ctx) => {
-        if (!ctx.session?.user?.id) throw new Error("Unauthorized");
-        console.log("ctx.session.user.id", ctx.session.user.id);
-        return {
-          ...input,
-          userId: ctx.session.user.id,
-        };
-      },
-    },
-    table: eoas,
-    db: db,
-  }),
-  safes: createRouterFactory({
-    id: "safes",
-    selectSchema: selectSafeSchema,
-    updateSchema: selectSafeSchema.partial().extend({ id: z.string() }),
-    insertSchema: insertSafeSchema,
-    config: {
-      eoas: {
-        relation: "true",
-      },
-    },
-    hooks: {
-      beforeInsert: (input, ctx) => {
-        if (!ctx.session?.user?.id) throw new Error("Unauthorized");
-        return {
-          ...input,
-          userId: ctx.session.user.id,
-        };
-      },
-    },
-    table: safes,
-    db: db,
-  }),
   verifyWallet: verifyWalletRouter,
   zeroEx: ZeroExRouter,
   stores: storeRouter,
