0c6b5c0cb0fb3494ff4c3e0b3ed7589c8974ce2a
Ayrton Paredes <ayrton@thinkanddev.com>
2024-01-19 20:36:55 -0400

    folder name

--- apps/ghommerce/src/components/web3/ChainInfo.tsx
+++ apps/ghommerce/src/components/web3/ChainInfo.tsx
@@ -7,7 +7,7 @@ export const ChainInfo = (props: {
     <div className="flex flex-row items-center space-x-2">
       <div className="w-8 h-8 rounded-full bg-gray-200 flex items-center justify-center">
         <img
-          src={`/icons/${ChainIdToName[props.chainId]}.svg`}
+          src={`/chain-logo/${ChainIdToName[props.chainId]}.svg`}
           className="w-6 h-6"
           alt={ChainIdToName[props.chainId]}
         />
